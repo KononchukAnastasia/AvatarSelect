f20296ad61819213e395a8cdbf0b57cecba36404
Anastasia Kononchuk <nastyakononchuk@me.com>
2024-07-15 23:09:10 +0700

    Initial Commit

---
+++ AvatarSelect/AvatarSelectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvatarSelectApp.swift
+//  AvatarSelect
+//
+//  Created by Анастасия Конончук on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AvatarSelectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
